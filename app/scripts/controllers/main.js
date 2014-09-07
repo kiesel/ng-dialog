478763462655825c9e5612bf35061063c8fd6901
Alex Kiesel <alex@kiesel.name>
2014-09-07 10:40:24 +0000

    Created app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dialogAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dialogAngularApp
+ */
+angular.module('dialogAngularApp')
+  .controller('MainCtrl', function ($scope) {
+  });
